375ff2afd54721520bffb85b8b227b539085ee5a
Michiel Borkent <michielborkent@gmail.com>
2020-12-28 12:45:15 +0100

    etaoin

---
+++ examples/etaoin.clj
@@ -0,0 +1,13 @@
+#!/usr/bin/env bb
+
+(require '[babashka.pods :as pods])
+
+(pods/load-pod 'org.babashka/etaoin "0.0.1")
+
+(require '[pod.babashka.etaoin :as eta])
+
+(def driver (eta/firefox))
+
+(eta/go driver "https://clojure.org")
+
+(eta/quit driver)
