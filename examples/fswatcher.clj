3396c54b89090d37fcbc3c08ba341ceb09d79fcc
Michiel Borkent <michielborkent@gmail.com>
2020-12-29 16:34:13 +0100

    fswatcher 0.0.2

--- examples/fswatcher.clj
+++ examples/fswatcher.clj
@@ -1,7 +1,7 @@
 #!/usr/bin/env bb
 
 (require '[babashka.pods :as pods])
-(pods/load-pod 'org.babashka/fswatcher "0.0.1")
+(pods/load-pod 'org.babashka/fswatcher "0.0.2")
 
 (require '[pod.babashka.fswatcher :as fw])
 
