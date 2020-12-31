e0cd72451b299cab5d80177eaf2af934f58c093f
Michiel Borkent <michielborkent@gmail.com>
2020-12-31 16:52:13 +0100

    example

--- examples/sqlite3.clj
+++ examples/sqlite3.clj
@@ -1,5 +1,9 @@
 (ns sqlite3-script
-  (:require [babashka.deps :as deps]))
+  (:require [babashka.pods :as pods]))
 
 (pods/load-pod 'org.babashka/sqlite3 "0.0.1")
 (require '[pod.babashka.sqlite3 :as sqlite])
+
+(sqlite/execute! "/tmp/foo.db" ["create table foo (bar)"])
+(sqlite/execute! "/tmp/foo.db" ["insert into foo (bar) (?)" "baz"])
+(sqlite/query    "/tmp/foo.db" ["select * from foo"])
