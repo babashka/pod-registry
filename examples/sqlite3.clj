5720698831672f1d6210a5ef068589744089b451
Michiel Borkent <michielborkent@gmail.com>
2020-12-31 17:39:40 +0100

    Fix sqlite example

--- examples/sqlite3.clj
+++ examples/sqlite3.clj
@@ -1,3 +1,5 @@
+#!/usr/bin/env bb
+
 (ns sqlite3-script
   (:require [babashka.pods :as pods]))
 
@@ -5,5 +7,5 @@
 (require '[pod.babashka.sqlite3 :as sqlite])
 
 (sqlite/execute! "/tmp/foo.db" ["create table foo (bar)"])
-(sqlite/execute! "/tmp/foo.db" ["insert into foo (bar) (?)" "baz"])
+(sqlite/execute! "/tmp/foo.db" ["insert into foo (bar) values (?)" "baz"])
 (sqlite/query    "/tmp/foo.db" ["select * from foo"])
