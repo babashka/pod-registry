96fbd97da9281c7937605ccc4cda2af5a830f55f
Michiel Borkent <michielborkent@gmail.com>
2021-01-06 13:13:17 +0100

    Add aws pod manifest

---
+++ examples/aws.clj
@@ -0,0 +1,11 @@
+#!/usr/bin/env bb
+
+(require '[babashka.pods :as pods])
+
+(pods/load-pod 'org.babashka/aws "0.0.1")
+
+(require '[pod.babashka.aws :as aws])
+
+(def s3-client (aws/client {:api :s3}))
+
+(aws/doc s3-client :ListBuckets)
