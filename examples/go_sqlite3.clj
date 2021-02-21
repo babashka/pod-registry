1abf4af9693ee7e1f0d55a2916a35514310fac1a
Michiel Borkent <michielborkent@gmail.com>
2021-02-21 12:26:18 +0100

    Rename sqlite3 pod to go-sqlite3

--- examples/go_sqlite3.clj
+++ examples/go_sqlite3.clj
@@ -3,8 +3,8 @@
 (ns sqlite3-script
   (:require [babashka.pods :as pods]))
 
-(pods/load-pod 'org.babashka/sqlite3 "0.0.1")
-(require '[pod.babashka.sqlite3 :as sqlite])
+(pods/load-pod 'org.babashka/go-sqlite3 "0.0.1")
+(require '[pod.babashka.go-sqlite3 :as sqlite])
 
 (sqlite/execute! "/tmp/foo.db" ["create table foo (bar)"])
 (sqlite/execute! "/tmp/foo.db" ["insert into foo (bar) values (?)" "baz"])
