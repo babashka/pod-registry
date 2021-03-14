b3be0402ca05d9553169595c2bf9271c5f809131
Michiel Borkent <michielborkent@gmail.com>
2021-03-14 20:02:29 +0100

    sql pods 0.0.5

---
+++ script/upgrade-manifest.clj
@@ -0,0 +1,36 @@
+#!/usr/bin/env bb
+
+(ns upgrade-manifest
+  (:require [babashka.deps :as deps]
+            [babashka.fs :as fs]
+            [clojure.string :as str]))
+
+;; TODO: version-clj 2.0 isn't compatible with bb anymore
+(deps/add-deps '{:deps {version-clj/version-clj {:mvn/version "2.0.1"}}})
+
+(require '[version-clj.core :as v])
+
+(defn latest-version [pod]
+  (let [pod-dir (fs/path "manifests" pod)
+        dirs (map fs/file-name (fs/list-dir pod-dir))
+        latest (reduce (fn [acc v]
+                         (if (v/newer? v acc)
+                           v acc))
+                       (first dirs)
+                       (rest dirs))]
+    latest))
+
+(defn upgrade-manifest [pod version]
+  (let [latest (latest-version pod)
+        pod-dir (fs/path "manifests" pod)
+        latest-dir (fs/path pod-dir latest)
+        target (fs/path pod-dir version)]
+    (fs/create-dirs target) ;; copy-tree doesn't create the dir, TODO: fix?
+    (fs/copy-tree latest-dir target)
+    (doseq [f (fs/list-dir target)]
+      (spit (fs/file f) (str/replace (slurp (fs/file f))
+                                     latest version)))))
+
+(when (= *file* (System/getenv "babashka.file"))
+  (let [[pod version] *command-line-args*]
+    (upgrade-manifest pod version)))
---
+++ script/upgrade-sql-pods.clj
@@ -0,0 +1,22 @@
+#!/usr/bin/env bb
+
+(ns upgrade-sql-pods
+  (:require [clojure.string :as str]))
+
+(load-file "script/upgrade-manifest.clj")
+(require 'upgrade-manifest)
+
+(def version (first *command-line-args*))
+(assert version "No version")
+
+(def latest (upgrade-manifest/latest-version "org.babashka/hsqldb"))
+
+(upgrade-manifest/upgrade-manifest "org.babashka/hsqldb" version)
+(upgrade-manifest/upgrade-manifest "org.babashka/postgresql" version)
+(upgrade-manifest/upgrade-manifest "org.babashka/mssql" version)
+
+(def hsqldb-example "examples/hsqldb.clj")
+
+(spit hsqldb-example
+      (str/replace (slurp hsqldb-example)
+                   latest version))
