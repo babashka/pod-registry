b28a430aa8774ee976c8aa2732a1e380c37bc778
Huahai Yang <huahai.yang@gmail.com>
2021-03-18 16:04:39 -0700

    add datalevin

---
+++ examples/datalevin.clj
@@ -0,0 +1,15 @@
+#!/usr/bin/env bb
+
+(require '[babashka.pods :as pods])
+
+(pods/load-pod 'huahaiy/datalevin "0.4.28")
+
+(require '[pod.huahaiy.datalevin :as d])
+
+(def conn (d/get-conn "/tmp/mydb"))
+
+(d/transact! conn [{:greeting "Hello world!"}])
+
+(d/q '[:find ?g :where [_ :greeting ?g]] (d/db conn))
+
+(d/close conn)
