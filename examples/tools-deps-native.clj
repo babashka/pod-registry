dd5869dbbc26d3060949c4c3cd0cf4918de982c0
Michiel Borkent <michielborkent@gmail.com>
2021-11-28 17:26:44 +0100

    tools-deps-native 0.0.5

--- examples/tools-deps-native.clj
+++ examples/tools-deps-native.clj
@@ -1,6 +1,6 @@
 (require '[babashka.pods :as pods])
 
-(pods/load-pod 'org.babashka/tools-deps-native "0.0.4")
+(pods/load-pod 'org.babashka/tools-deps-native "0.0.5")
 
 (require '[clojure.tools.deps.alpha :as tda])
 
@@ -9,4 +9,4 @@
      :classpath-roots
      (take 2))
 
-;;= > ("src" "/Users/borkdude/.m2/repository/org/clojure/clojure/1.10.1/clojure-1.10.1.jar")
+;;=> ("src" "/Users/borkdude/.m2/repository/buddy/buddy-core/1.10.1/buddy-core-1.10.1.jar")
