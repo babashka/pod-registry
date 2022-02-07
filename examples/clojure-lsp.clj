22f3ddb146328be989c88ac1ad2d27926cf634f1
Michiel Borkent <michielborkent@gmail.com>
2022-02-07 11:23:35 +0100

    Update clojure-lsp

--- examples/clojure-lsp.clj
+++ examples/clojure-lsp.clj
@@ -2,7 +2,7 @@
   (:require
    [babashka.pods :as pods]))
 
-(pods/load-pod 'com.github.clojure-lsp/clojure-lsp "2022.01.03-19.46.10")
+(pods/load-pod 'com.github.clojure-lsp/clojure-lsp "2022.02.01-20.02.32")
 
 (require '[clojure-lsp.api :as api])
 ;; see https://cljdoc.org/d/com.github.clojure-lsp/clojure-lsp/2021.12.20-00.36.56/api/clojure-lsp.api
