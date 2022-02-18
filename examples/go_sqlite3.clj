c9166a9dae1dafc11448b1eae0ae0e70b03e5d57
Michiel Borkent <michielborkent@gmail.com>
2022-02-18 11:29:48 +0100

    go-sqlite3 0.1.0

--- examples/go_sqlite3.clj
+++ examples/go_sqlite3.clj
@@ -4,7 +4,7 @@
   (:require
    [babashka.pods :as pods]))
 
-(pods/load-pod 'org.babashka/go-sqlite3 "0.0.1")
+(pods/load-pod 'org.babashka/go-sqlite3 "0.1.0")
 (require '[pod.babashka.go-sqlite3 :as sqlite])
 
 (sqlite/execute! "/tmp/foo.db" ["create table foo (bar)"])
