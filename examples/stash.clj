71c7f5a66a39ec954ed68246041ae40b44673584
Michiel Borkent <michielborkent@gmail.com>
2022-03-20 21:01:07 +0100

    Add stash example

---
+++ examples/stash.clj
@@ -0,0 +1,15 @@
+(require '[babashka.pods :as pods])
+
+(pods/load-pod 'rorokimdim/stash "0.3.1")
+
+(require '[pod.rorokimdim.stash :as stash])
+
+(stash/init {"encryption-key" "foo"
+             "stash-path" "foo.stash"
+             "create-stash-if-missing" true})
+
+(prn (stash/version))
+
+(stash/set :foo :bar "baz")
+
+(prn (stash/get :foo :bar))
