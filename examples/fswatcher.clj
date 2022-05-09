783c13c776392a85ae626d07b7dc8fe37db5a282
Michiel Borkent <michielborkent@gmail.com>
2022-05-09 21:14:37 +0200

    Bump fswatcher

--- examples/fswatcher.clj
+++ examples/fswatcher.clj
@@ -1,11 +1,11 @@
 #!/usr/bin/env bb
 
 (require '[babashka.pods :as pods])
-(pods/load-pod 'org.babashka/fswatcher "0.0.2")
+(pods/load-pod 'org.babashka/fswatcher "0.0.3")
 
 (require '[pod.babashka.fswatcher :as fw])
 
-(fw/watch "." (fn [event] (prn event)) {:delay-ms 5000})
+(fw/watch "." prn {:delay-ms 5000})
 
 (println "Watching current directory for changes... Press Ctrl-C to quit.")
 
