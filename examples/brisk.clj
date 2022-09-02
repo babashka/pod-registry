d5ef95a3792b137fda026640c271cc745f39e8f5
Nate Jones <nate@endot.org>
2022-09-02 13:10:02 -0700

    Brisk 0.2.1 (#38)

--- examples/brisk.clj
+++ examples/brisk.clj
@@ -2,9 +2,15 @@
 
 (require '[babashka.pods :as pods])
 
-(pods/load-pod 'justone/brisk "0.2.0")
+(pods/load-pod 'justone/brisk "0.2.1")
 
 (require '[pod.brisk :as brisk])
 
+;; from/to file
 (brisk/freeze-to-file "pod.nippy" {:han :solo})
 (prn (brisk/thaw-from-file "pod.nippy"))
+
+;; from/to string
+(let [frozen (brisk/freeze-to-string {:princess :leia})]
+  (println "frozen:" frozen)
+  (prn (brisk/thaw-from-string frozen)))
