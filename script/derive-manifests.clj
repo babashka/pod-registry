b5c17e4a551415629359cbf5885b29e586b1416f
vollcheck <jaw@juxt.pro>
2022-10-02 13:19:49 +0200

    script for derivation is complete

--- script/derive-manifests.clj
+++ script/derive-manifests.clj
@@ -1,10 +1,14 @@
 #!/usr/bin/env bb
 
 (ns derive-manifests
-  (:require [clojure.edn :as edn]
+  (:require [babashka.deps :as deps]
+            [clojure.edn :as edn]
             [clojure.java.io :as io]
-            [clojure.string :as str]
-            [hiccup2.core :as html]))
+            [clojure.string :as str]))
+
+;; Install version-clj on the classpath
+(deps/add-deps '{:deps {version-clj/version-clj {:mvn/version "2.0.2"}}})
+(require '[version-clj.core :as v])
 
 (defn edn-read
   [path]
@@ -16,34 +20,56 @@
        file-seq
        (filter #(str/ends-with? % "manifest.edn"))
        (map edn-read)
-       (sort-by :pod/version)
-       (reverse)
+
+       ;; reversed because we want higher to lower
+       (sort (fn [x y] (v/version-seq-compare (:pod/version y) (:pod/version x))))
+
+       ;; get first of the pods group only
        (group-by :pod/name)
        (reduce (fn [agg [_ vs]] (conj agg (first vs))) [])))
 
-(def table
-  ;; This can be done using either `hiccup` or `clojure.pprint/cl-format`
-  (html/html [:table
-              [:tr
-               [:th "Pod"]
-               [:th "Description"]
-               [:th "Latest version"]]
-              (for [pod recent-manifests
-                    :let [{name :pod/name
-                           description :pod/description
-                           version :pod/version} pod]]
-                [:tr
-                 [:td name]
-                 [:td description]
-                 [:td version]])]))
+(defn format-name
+  [pod]
+  (->> pod
+       :pod/artifacts
+       first
+       :artifact/url
+       (re-find #"https:\/\/github\.com\/[A-Za-z0-9-]*\/[A-Za-z0-9-]*")
+       (format "[%s](%s)" (:pod/name pod))))
+
+(def ^:const programming-langs
+  {:clojure "[<img src=\"https://upload.wikimedia.org/wikipedia/commons/5/5d/Clojure_logo.svg\" alt=\"clojure\" width=\"24\" height=\"24\">](https://clojure.org/)"
+   :go "[<img src=\"https://go.dev/blog/go-brand/Go-Logo/SVG/Go-Logo_Blue.svg\" alt=\"golang\" width=\"24\" height=\"24\">](https://golang.org/)"
+   :rust "[<img src=\"https://upload.wikimedia.org/wikipedia/commons/d/d5/Rust_programming_language_black_logo.svg\" alt=\"rust\" width=\"24\" height=\"24\">](https://www.rust-lang.org/)"
+   :haskell "[<img src=\"https://upload.wikimedia.org/wikipedia/commons/1/1c/Haskell-Logo.svg\" alt=\"haskell\" width=\"24\" height=\"24\">](https://haskell.org/)"})
+
+(defn generate-table
+  [pods]
+  (reduce
+   (fn [table {:pod/keys [description version example language] :as p}]
+     (str table
+          (format "| %s | %s | %s | %s | %s |\n"
+                  (format-name p)
+                  description
+                  version
+                  (format "[link](%s)" example)
+                  (programming-langs (keyword language)))))
+   (str "| Pod | Description | Latest version | Example | Language |\n"
+        "| --- | --- | --- | --- | --- |\n")
+   pods))
 
 (defn insert-table!
-  [table]
+  []
   (let [readme-path "README.md"
         readme-content (slurp readme-path)
-        new-content (str/replace readme-content
-                                 #"## Registered pods\n\n"
-                                 (str "## Registered pods\n\n" table "\n\n"))]
+        table (generate-table recent-manifests)
+
+        new-content
+        (str/replace readme-content
+                     ;; if there's table already, replace it
+                     ;; #"## Registered pods\n\n.*\n\n## Registering a pod"
+                     #"\#\# Registered pods\n\n(.*\|\n)*"
+                     (str "## Registered pods\n\n" table))]
     (spit readme-path new-content)))
 
-(insert-table! table)
+(insert-table!)
