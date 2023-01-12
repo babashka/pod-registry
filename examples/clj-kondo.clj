8fb0f07c6f4166b5007f77fddd9eb1f273d9fdb1
Michiel Borkent <michielborkent@gmail.com>
2023-01-12 15:50:20 +0100

    clj-kondo

--- examples/clj-kondo.clj
+++ examples/clj-kondo.clj
@@ -2,7 +2,7 @@
 
 (require '[babashka.pods :as pods])
 
-(pods/load-pod 'clj-kondo/clj-kondo "2022.12.08")
+(pods/load-pod 'clj-kondo/clj-kondo "2023.01.12")
 
 (require '[pod.borkdude.clj-kondo :as clj-kondo])
 
