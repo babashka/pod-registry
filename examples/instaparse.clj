e9ec4115478679c15d0561d2cca56d5f6aec00e7
Michiel Borkent <michielborkent@gmail.com>
2023-01-14 15:18:02 +0100

    instaparse manifest

---
+++ examples/instaparse.clj
@@ -0,0 +1,23 @@
+(ns instaparse)
+
+(require '[babashka.pods :as pods])
+
+(pods/load-pod 'org.babashka/instaparse "0.0.1")
+
+(require '[pod.babashka.instaparse :as insta])
+
+(def as-and-bs
+  (insta/parser
+   "S = AB*
+    AB = A B
+    A = 'a'+
+    B = 'b'+"))
+
+#_(prn (as-and-bs "aaaaabbbaaaabb"))
+
+(prn (insta/parse as-and-bs "aaaaabbbaaaabb"))
+
+(def failure (insta/parse as-and-bs "xaaaaabbbaaaabb"))
+
+(prn failure)
+(prn :failure (insta/failure? failure))
