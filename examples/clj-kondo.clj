c1f1da044fdef7195b963a9d35a316f2876a4da9
Michiel Borkent <michielborkent@gmail.com>
2023-02-17 13:44:09 +0100

    Revert "Upgrade clj-kondo pod:"
    This reverts commit b0b80d5bcfed50ce9530bda5ce4dfe9102f6e5f5.

--- examples/clj-kondo.clj
+++ examples/clj-kondo.clj
@@ -2,7 +2,7 @@
 
 (require '[babashka.pods :as pods])
 
-(pods/load-pod 'clj-kondo/clj-kondo "2023.02.17")
+(pods/load-pod 'clj-kondo/clj-kondo "2023.01.12")
 
 (require '[pod.borkdude.clj-kondo :as clj-kondo])
 
