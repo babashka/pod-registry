bf31961b34a6b15ac805045b785ed20f204ca366
Michiel Borkent <michielborkent@gmail.com>
2023-07-13 21:36:48 +0200

    upgrade example

--- examples/clj-kondo.clj
+++ examples/clj-kondo.clj
@@ -2,7 +2,7 @@
 
 (require '[babashka.pods :as pods])
 
-(pods/load-pod 'clj-kondo/clj-kondo "2023.05.26")
+(pods/load-pod 'clj-kondo/clj-kondo "2023.07.13")
 
 (require '[pod.borkdude.clj-kondo :as clj-kondo])
 
