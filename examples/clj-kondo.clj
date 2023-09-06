be2ceaf296033882932ce41a78e82f1af8fdff5c
Michiel Borkent <michielborkent@gmail.com>
2023-09-06 21:07:28 +0200

    manifest

--- examples/clj-kondo.clj
+++ examples/clj-kondo.clj
@@ -2,7 +2,7 @@
 
 (require '[babashka.pods :as pods])
 
-(pods/load-pod 'clj-kondo/clj-kondo "2023.07.13")
+(pods/load-pod 'clj-kondo/clj-kondo "2023.09.07")
 
 (require '[pod.borkdude.clj-kondo :as clj-kondo])
 
