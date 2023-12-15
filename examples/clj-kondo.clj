b77eee1123d47667d2a51ce96c796306c728e765
Michiel Borkent <michielborkent@gmail.com>
2023-12-15 15:25:17 +0100

    Bump README

--- examples/clj-kondo.clj
+++ examples/clj-kondo.clj
@@ -2,7 +2,7 @@
 
 (require '[babashka.pods :as pods])
 
-(pods/load-pod 'clj-kondo/clj-kondo "2023.10.20")
+(pods/load-pod 'clj-kondo/clj-kondo "2023.12.15")
 
 (require '[pod.borkdude.clj-kondo :as clj-kondo])
 
