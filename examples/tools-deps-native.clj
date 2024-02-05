a8738d3565f318177568273114f598143f9a5295
Michiel Borkent <michielborkent@gmail.com>
2024-02-05 17:19:01 +0100

    it works

--- examples/tools-deps-native.clj
+++ examples/tools-deps-native.clj
@@ -1,12 +1,17 @@
-(require '[babashka.pods :as pods])
+(require '[babashka.pods :as pods]
+         '[babashka.fs :as fs])
 
-(pods/load-pod 'org.babashka/tools-deps-native "0.1.2")
+(pods/load-pod 'org.babashka/tools-deps-native "0.1.4")
 
 (require '[clojure.tools.deps :as td])
 
-(->> (td/create-basis {:project "deps.edn"
+(def current-file *file*)
+
+
+(->> (td/create-basis {:project (str (fs/file (fs/parent *file*) "tools-deps-native.edn"))
                        :extra '{:deps {buddy/buddy-core {:mvn/version "1.10.1"}}}})
      :classpath-roots
-     (take 2))
+     (take 2)
+     prn)
 
 ;;=> ("src" "/Users/borkdude/.m2/repository/buddy/buddy-core/1.10.1/buddy-core-1.10.1.jar")
