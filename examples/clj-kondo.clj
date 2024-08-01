6c6865e4b41a0b0a672222adc4c20c6afece2d4c
Michiel Borkent <michielborkent@gmail.com>
2024-08-01 13:30:06 +0200

    update example

--- examples/clj-kondo.clj
+++ examples/clj-kondo.clj
@@ -2,7 +2,7 @@
 
 (require '[babashka.pods :as pods])
 
-(pods/load-pod 'clj-kondo/clj-kondo "2024.05.22")
+(pods/load-pod 'clj-kondo/clj-kondo "2024.08.01")
 
 (require '[pod.borkdude.clj-kondo :as clj-kondo])
 
