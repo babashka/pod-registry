89fec22df53397db1e9ea410b57a9ab723c1797e
Michiel Borkent <michielborkent@gmail.com>
2025-01-16 16:49:40 +0100

    derive manifests

--- examples/clj-kondo.clj
+++ examples/clj-kondo.clj
@@ -2,7 +2,7 @@
 
 (require '[babashka.pods :as pods])
 
-(pods/load-pod 'clj-kondo/clj-kondo "2024.08.29")
+(pods/load-pod 'clj-kondo/clj-kondo "2025.01.16")
 
 (require '[pod.borkdude.clj-kondo :as clj-kondo])
 
