29d9369fa444a10d78c353352e08c1f8ce743614
Timo Kramer <fw-git@timokramer.de>
2025-08-27 12:36:49 +0200

    Add Datahike pod in version 0.6.1601

---
+++ examples/datahike.clj
@@ -0,0 +1,42 @@
+#!/usr/bin/env bb
+
+(require '[babashka.pods :as pods])
+
+(pods/load-pod 'replikativ/datahike "0.6.1601")
+
+(require '[datahike.pod :as d])
+
+(def config {:store  {:backend :file
+                      :path "/tmp/bb-datahike-pod"}
+             :keep-history? true
+             :schema-flexibility :read})
+
+(d/delete-database config)
+
+(d/create-database config)
+
+(def conn (d/connect config))
+
+(d/transact conn [{:name  "Alice", :age   20}
+                  {:name  "Bob", :age   30}
+                  {:name  "Charlie", :age   40}
+                  {:age 15}])
+
+(def db (d/db conn))
+
+(d/q '[:find ?e ?n ?a
+       :where
+       [?e :name ?n]
+       [?e :age ?a]]
+     db)
+
+(d/release-db db)
+
+(d/pull (d/db conn) '[*] 3)
+
+(d/with-db [db (d/db conn)]
+  (d/q {:query '{:find [?e ?n ?a]
+                 :where
+                 [[?e :name ?n]
+                  [?e :age ?a]]}
+        :args [db]}))
