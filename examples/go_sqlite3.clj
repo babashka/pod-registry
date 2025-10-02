934404e81e79ce24dbcbe78069457df54d082e3b
Michiel Borkent <michielborkent@gmail.com>
2025-10-02 23:53:22 +0200

    example sqlite

--- examples/go_sqlite3.clj
+++ examples/go_sqlite3.clj
@@ -5,10 +5,11 @@
    [babashka.fs :as fs]
    [babashka.pods :as pods]))
 
-(pods/load-pod 'org.babashka/go-sqlite3 "0.3.10")
+(pods/load-pod 'org.babashka/go-sqlite3 "0.3.12")
 (require '[pod.babashka.go-sqlite3 :as sqlite])
 
 (when-not (fs/exists? "/tmp/foo.db")
   (sqlite/execute! "/tmp/foo.db" ["create table foo (bar)"]))
-(sqlite/execute! "/tmp/foo.db" ["insert into foo (bar) values (?)" "baz"])
-(sqlite/query    "/tmp/foo.db" ["select * from foo"])
+(let [conn (sqlite/get-connection "/tmp/foo.db")]
+  (sqlite/execute! conn ["insert into foo (bar) values (?)" "baz"])
+  (prn (sqlite/query conn ["select * from foo"])))
